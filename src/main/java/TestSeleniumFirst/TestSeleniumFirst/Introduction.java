c407651def7d7f92639a020b4b13891a086af8dd
sayalizagade <39559819+sayalizagade@users.noreply.github.com>
2024-04-03 17:06:36 +0530

    Test

--- src/main/java/TestSeleniumFirst/TestSeleniumFirst/Introduction.java
+++ src/main/java/TestSeleniumFirst/TestSeleniumFirst/Introduction.java
@@ -1,20 +1,22 @@
 package TestSeleniumFirst.TestSeleniumFirst;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Introduction {
+	
+public static WebDriver driver;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
-		//Invoking Browser
-		ChromeDriver driver = new ChromeDriver();
+		 driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/");
 		//driver.getTitle();
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
 		driver.close();
-;	}
+		System.out.println("TestSayali");	
+		
+	}
 
 }
